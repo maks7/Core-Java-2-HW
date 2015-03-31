ea0fb8e1171747af9e78562678c3837ce5b89fd8
maks7 <maxim13bg@mail.bg>
2015-03-31 10:20:17 +0300

    updated StackImpl and test

--- W1-OOPFun/src/com/hackbulgaria/oopfun4/stackimpl/StackImpl.java
+++ W1-OOPFun/src/com/hackbulgaria/oopfun4/stackimpl/StackImpl.java
@@ -1,55 +1,164 @@
 package com.hackbulgaria.oopfun4.stackimpl;
 
+import java.util.EmptyStackException;
 import java.util.LinkedList;
 
 public class StackImpl implements Stack {
 
     protected LinkedList ll;
     
+    private int stackCapacity = 16;
+    public Object[] array;
+    
     public StackImpl() {
-        this.ll = new LinkedList();
+        array = new Object[stackCapacity-1];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = null;
+        }
     }
-
+    
+    private Object[] extendArray(int len, Object o) {
+        Object[] newArray = new Object[len];
+        boolean isObjectAdded = false;
+        
+        for (int i = 0; i < newArray.length; i++) {
+            if (i < array.length) {
+                newArray[i] = array[i];
+            } else {
+                if (!isObjectAdded) { 
+                    newArray[i] = o;
+                    isObjectAdded = true;
+                } else {
+                    newArray[i] = null;
+                }
+            }
+        }
+        
+        return newArray;
+    }
+    
     @Override
     public void push(Object obj) {
-        ll.push(obj);
+
+        boolean isDone = false;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                array[i] = obj;
+                isDone = true;
+                break;
+            }
+            
+//            int test1 = array.length;
+//            Object testOb3 = array[array.length - 1];
+            
+//            try {
+//                array[array.length] 
+//            } catch () {
+//                array = extendArray(array.length + stackCapacity, obj);
+//            }
+//            if (array[array.length - 1] != null) {
+////                Object[] extendedArray = new Object[array.length + stackCapacity];
+////                array = extendedArray;
+//               array = extendArray(array.length + stackCapacity, obj);
+           // }
+        }
     }
 
     @Override
     public Object peek() {
-        return (Object) ll.peek();
+        Object result = null;
+
+        try {
+            for (int i = array.length - 1; i > -2; i--) {
+
+                if (array[i] != null) {
+                    result = array[i];
+                    break;
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new EmptyStackException();
+        }
+
+        return result;
     }
 
     @Override
     public Object pop() {
-        return (Object) ll.pop();
+
+        Object result = null;
+
+        try {
+            for (int i = array.length - 1; i > -2; i--) {
+
+                if (array[i] != null) {
+                    result = array[i];
+                    array[i] = null;
+                    break;
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new EmptyStackException();
+        }
+
+        return result;
     }
 
     @Override
     public void clear() {
-        ll.clear();
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = null;
+        }
      }
 
     @Override
     public boolean isEmpty() {
-        if(ll.size() == 0)
-            return true;
-        else {
-            return false;
+
+        boolean result = true;
+        int i = 0;
+
+        while (i < array.length) {
+            if (array[i] != null) {
+                result = false;
+                break;
+            }
+            i++;
         }
+
+        return result;
     }
 
     @Override
     public int length() {
-        return ll.size();
+        int len = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                len++;
+            }
+        }
+        
+        return len;
     }
     
     @Override
     public int search(Object o) {
-        if(ll.contains(o)) {
-            return ll.indexOf(o);
-        } else {
-            return -1;
+       
+        int result = -1;
+        int position = 0;
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] != null) {
+                if (array[i].equals(o)) {
+                    result = position;
+                    break;
+                }
+                position++;
+            }
         }
+        
+        return result;
     }
 }
--- W1-OOPFun/test/com/hackbulgaria/oopfun/OOPFun4StackImplTest.java
+++ W1-OOPFun/test/com/hackbulgaria/oopfun/OOPFun4StackImplTest.java
@@ -2,6 +2,8 @@ package com.hackbulgaria.oopfun;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,34 +11,57 @@ import com.hackbulgaria.oopfun4.stackimpl.StackImpl;
 
 public class OOPFun4StackImplTest {
 
-    StackImpl ll;
+    StackImpl st;
     
     @Before
     public void before() {
         
-        this.ll = new StackImpl();
+        st = new StackImpl();
+    }
+    
+    @Test
+    public void testPush_with_Capacity_gt_than_18() {
+        
+        Random ran = new Random(50);
+        Object testO = new Integer(ran.nextInt());
+        
+      //  for (int i = 0; i < 5; i++) {
+            st.push(new Integer(2));
+            st.push(new Integer(4));
+            st.push(new Integer(8));
+            st.push(new Integer(16));
+            st.push(new Integer(32));
+            st.push(new Integer(64));
+         //   System.out.println(i);
+           // assertEquals(i+1, st.length());
+     //   }
+        
+        assertEquals(new Integer(8), st.array[2]);
+        
+        
+       // assertEquals(20, st.length());
     }
     
     @Test
     public void testLength_0() {
         
-        assertEquals(0, ll.length());
+        assertEquals(0, st.length());
     }
     
     @Test
     public void testLength_1() {
         
-        ll.push(new Integer(5));
-        assertEquals(1, ll.length());
+        st.push(new Integer(5));
+        assertEquals(1, st.length());
     }
 
     @Test
     public void testLength_3() {
         
-        ll.push(new Integer(5));
-        ll.push(new Integer(50));
-        ll.push(new Integer(15));
-        assertEquals(3, ll.length());
+        st.push(new Integer(5));
+        st.push(new Integer(50));
+        st.push(new Integer(15));
+        assertEquals(3, st.length());
     }
     
     @Test
@@ -44,76 +69,89 @@ public class OOPFun4StackImplTest {
 
         Object element = new Integer(7);
         
-        assertEquals(0, ll.length());
-        ll.push(element);
-        assertEquals(1, ll.length());
+        assertEquals(0, st.length());
+        st.push(element);
+        assertEquals(1, st.length());
     }
 
+    @Test
+    public void testPush_Add_3_elements() {
+
+        Object element = new Integer(7);
+        
+       // st.push(element);
+        st.push(new Integer(5));
+        st.push(new Integer(50));
+        
+        assertEquals(50, st.peek());
+    }
+
+    
     @Test
     public void testPeek() {
 
         Object element = new Integer(17);
-        ll.push(element);
+        st.push(element);
                 
-        assertEquals(1, ll.length());
-        assertEquals(element, ll.peek());
-        assertEquals(1, ll.length());
+        assertEquals(1, st.length());
+        assertEquals(element, st.peek());
+        assertEquals(1, st.length());
     }
 
     @Test
     public void testPop() {
 
         Object element = new Integer(7);
-        ll.push(element);
+        st.push(element);
         
-        assertEquals(1, ll.length());
-        assertEquals(element, ll.pop());
-        assertEquals(0, ll.length());
+        assertEquals(1, st.length());
+        assertEquals(element, st.pop());
+        assertEquals(0, st.length());
     }
 
     @Test
     public void testClear() {
 
         Object element = new Integer(7);
-        ll.push(element);
-        ll.push(new Integer(18));
-        ll.push(new Integer(34));
+        st.push(element);
+        st.push(new Integer(18));
+        st.push(new Integer(34));
         
-        assertEquals(3, ll.length());
-        ll.clear();
-        assertEquals(0, ll.length());
+        assertEquals(3, st.length());
+        st.clear();
+        assertEquals(0, st.length());
         
     }
 
     @Test
     public void testIsEmpty() {
 
-        assertTrue(ll.isEmpty());
+        assertTrue(st.isEmpty());
     }
 
     @Test
     public void testIsEmpty_False() {
 
-        ll.push(new Integer(20));
-        assertFalse(ll.isEmpty());
+        st.push(new Integer(20));
+        assertFalse(st.isEmpty());
     }
 
     @Test
     public void testSearch_return0() {
         
         Object element = new Integer(20);
-        ll.push(element);
-        assertEquals(0, ll.search(element));
+        st.push(element);
+        assertEquals(0, st.search(element));
     }
 
     @Test
     public void testSearch_return2() {
 
         Object element = new Integer(20);
-        ll.push(element);
-        ll.push(new Integer(10));
-        ll.push(new Integer(15));
-        assertEquals(2, ll.search(element));
+        st.push(element);
+        st.push(new Integer(10));
+        st.push(new Integer(15));
+        assertEquals(2, st.search(element));
     }
 
 }
