8e7ea7ab3505016a854bf633d1be802ae4f37203
maks7 <maxim13bg@mail.bg>
2015-06-01 22:35:09 +0300

    update

---
+++ W6-1Multithreading/Threads/src/com/hackbulgaria/blockingqueue/BlockingQueue.java
@@ -0,0 +1,17 @@
+package com.hackbulgaria.blockingqueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BlockingQueue {
+
+    public static void main(String[] args) {
+
+        Queue blockingQueue = new LinkedList();
+        
+        public Object poll() {
+            
+        }
+    }
+
+}
